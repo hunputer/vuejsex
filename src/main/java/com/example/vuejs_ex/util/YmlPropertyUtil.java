1893df8efe3555b499ab930241b4d1f8028f01d4
hunputer <wlgns3855@gmail.com>
2023-09-13 22:16:49 +0900

    propertyEx3

--- src/main/java/com/example/vuejs_ex/util/YmlPropertyUtil.java
+++ src/main/java/com/example/vuejs_ex/util/YmlPropertyUtil.java
@@ -3,6 +3,7 @@ package com.example.vuejs_ex.util;
 import com.fasterxml.jackson.annotation.JacksonInject;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 
